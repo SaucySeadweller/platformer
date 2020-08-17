671baa8939d2bc7e42655f1c86177973cc2fe5ab
SaucySeadweller <37601822+SaucySeadweller@users.noreply.github.com>
2020-08-17 16:04:19 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 600
+	screenHeight = 800
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("initializing SDL ", err)
+		return
+	}
+	window, err := sdl.CreateWindow(
+		"Platformer",
+		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight,
+		sdl.WINDOW_OPENGL)
+	if err != nil {
+		fmt.Println("initializing window", err)
+		return
+	}
+	defer window.Destroy()
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("initializing renderer", err)
+	}
+	defer renderer.Destroy()
+}
